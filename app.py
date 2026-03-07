4ed9954189944a51b4c6fddc582fedaff9070aea
classicmoviesug <classicmoviesug@gmail.com>
2026-03-07 04:20:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -160,5 +160,8 @@ def payment_callback():
 def static_files(path):
     return send_from_directory("static", path)
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
+
+
 
